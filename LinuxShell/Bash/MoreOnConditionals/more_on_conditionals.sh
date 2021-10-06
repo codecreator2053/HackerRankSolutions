e88bad6be0b2dad513f1198334426762df35b086
codecreator2053 <54350201+codecreator2053@users.noreply.github.com>
2021-10-06 13:36:55 +0900

    fixed a bash solution file

--- LinuxShell/Bash/MoreOnConditionals/more_on_conditionals.sh
+++ LinuxShell/Bash/MoreOnConditionals/more_on_conditionals.sh
@@ -1,11 +1,11 @@
-#!bin/bash
-
 read X; read Y; read Z
+
+# echo "$X, $Y, $Z"
 
 if [[ $X == $Y ]] && [[ $X == $Z ]]
 then
     echo "EQUILATERAL"
-elif [ $X == $Y ] || [ $X == $Z ] || [ $Z == $Y ]
+elif [[ $X == $Y ]] || [[ $X == $Z ]] || [[ $Z == $Y ]]
 then
     echo "ISOSCELES"
 else
